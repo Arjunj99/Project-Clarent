1589390cd5b6edb1ad27b102ef32231835618e01
Arjunj99 <asj397@nyu.edu>
2019-12-30 11:19:11 +0900

    Initial Push

---
+++ HackNSlash0.5/Assets/Scripts/MovementManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A GameObject with this script has its movement controlable by the player.
+/// </summary>
+/// <author> Arjun Jaishankar </author>
+/// <version> 11/21/2019 </version>
+public class MovementManager : MonoBehaviour {
+    public CharacterController characterController;
+    
+    [Header("Acceleration Curves")]
+    public AnimationCurve forwardAnimation;
+    
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
